d200c18ddddca15776665989ca2447d8b18c58ba
Groutch <hurizen1@hotmail.com>
2018-07-08 19:31:24 +0200

    added MOAR colors

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var nameUser = localStorage.getItem("nameUser");
 if (colorUser != null) {
     document.body.style.color = colorUser;
     //document.querySelectorAll("a, h1, h2, h3, h4, h5, h6, p, span, ul, li").style.color = colorUser;
+    
     document.getElementById("my_carousel").className = "carousel slide my_carousel_"+nameUser;
     document.getElementById("sosimple").className = "row sosimple_"+nameUser;
+    document.getElementById("musicforward").className = "musicforward_"+nameUser;
 }
