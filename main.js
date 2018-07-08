74b6ab165f163a739b06ff9be493543e5bfbb868
Groutch <hurizen1@hotmail.com>
2018-07-08 17:17:29 +0200

    modifying color on user change

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ if (colorUser != null) {
     document.body.style.color = colorUser;
     //document.querySelectorAll("a, h1, h2, h3, h4, h5, h6, p, span, ul, li").style.color = colorUser;
     document.getElementById("my_carousel").className = "carousel slide my_carousel_"+nameUser;
+    document.getElementById("sosimple").className = "row sosimple_"+nameUser;
 }
